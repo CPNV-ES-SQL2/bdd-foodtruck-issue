b630f217456ebeeecde13dc6951d9e11277da3be
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2025-12-09 19:01:00 +0100

    feat: add detailed memory usage in script

--- appendices/sqlMonitor.py
+++ appendices/sqlMonitor.py
@@ -30,18 +30,31 @@ class MemoryProfiler:
         self.color_sequence = ['#ffc59b', '#d4c9fe', '#a9dffe', '#a9ecb8',
                                '#fff1a8', '#fbbfc7', '#fd812d', '#a18bf5',
                                '#47b7f8', '#40d763', '#f2b600', '#ff7082']
+        self.min_total = float('inf')
+        self.max_total = 0
         plt.rcParams['axes.xmargin'] = 0
         plt.rcParams['axes.ymargin'] = 0
-        plt.rcParams["font.family"] = "inter"
 
     def update_xy_axis(self, results, frequency):
         self.ms += frequency
         self.x.append(self.ms)
+        
+        sorted_results = sorted(results, key=lambda x: x[1], reverse=True)
+        
         if (len(self.y) == 0):
-            self.y = [[] for x in range(len(results))]
-        for i in range(len(results)-1, -1, -1):
-            usage = float(results[i][1]) / 1024
+            self.y = [[] for x in range(len(sorted_results))]
+        
+        total_at_point = 0
+        for i in range(len(sorted_results)):
+            usage = float(sorted_results[i][1]) / 1024
             self.y[i].append(usage)
+            total_at_point += usage
+        
+        if total_at_point < self.min_total:
+            self.min_total = total_at_point
+        if total_at_point > self.max_total:
+            self.max_total = total_at_point
+        
         if (len(self.x) > 50):
             self.x.pop(0)
             for i in range(len(self.y)):
@@ -49,30 +62,36 @@ class MemoryProfiler:
 
     def update_labels(self, results):
         total_mem = sum(map(lambda e: e[1], results))
+        sorted_results = sorted(results, key=lambda x: x[1], reverse=True)
         self.mem_labels.clear()
-        for i in range(len(results)-1, -1, -1):
-            usage = float(results[i][1]) / 1024
-            mem_type = results[i][0]
-            # Remove 'memory/' from beginning of name for brevity
+        for i in range(len(sorted_results)):
+            usage = float(sorted_results[i][1]) / 1024
+            mem_type = sorted_results[i][0]
             mem_type = mem_type[7:]
-            # Only show top memory users in legend
             if (usage < total_mem / 1024 / 50):
                 mem_type = '_' + mem_type
-            self.mem_labels.insert(0, mem_type)
+            label_with_usage = f"{mem_type} ({usage:.2f} KB)"
+            self.mem_labels.append(label_with_usage)
 
-    def draw_plot(self, plt):
+    def draw_plot(self, plt, current_results):
         plt.clf()
         plt.stackplot(self.x, self.y, colors = self.color_sequence)
         plt.legend(labels=self.mem_labels, bbox_to_anchor=(1.04, 1), loc="upper left", borderaxespad=0)
         plt.xlabel("milliseconds since monitor began")
         plt.ylabel("Kilobytes of memory")
+        
+        title_text = "Memory Usage"
+        if self.max_total > 0 and self.min_total < float('inf'):
+            diff = self.max_total - self.min_total
+            title_text += f" (Min-Max Difference: {diff:.2f} KB)"
+        plt.title(title_text, fontsize=12, pad=10)
 
     def configure_plot(self, plt):
         plt.ion()
-        fig = plt.figure(figsize=(12,5))
+        fig = plt.figure(figsize=(14,5))
         plt.stackplot(self.x, self.y, colors=self.color_sequence)
         plt.legend(labels=self.mem_labels, bbox_to_anchor=(1.04, 1), loc="upper left", borderaxespad=0)
-        plt.tight_layout(pad=4)
+        plt.subplots_adjust(right=0.7)
         return fig
 
     def start_visualization(self, database_connection, connection_id, frequency):
@@ -83,7 +102,7 @@ class MemoryProfiler:
             results = c.fetchall()
             self.update_xy_axis(results, frequency)
             self.update_labels(results)
-            self.draw_plot(plt)
+            self.draw_plot(plt, results)
             fig.canvas.draw_idle()
             fig.canvas.start_event_loop(frequency / 1000)
 
@@ -116,7 +135,7 @@ def main():
     connection_id = get_thread_for_connection_id(database_connection, args.connection_id)
     m = MemoryProfiler()
     m.start_visualization(database_connection, connection_id, args.frequency)
-    connection.close()
+    database_connection.close()
 
 if __name__ == "__main__":
     main()
